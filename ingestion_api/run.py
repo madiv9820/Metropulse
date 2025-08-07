849894a265d51fdfb5f00dae0b66a888a3748a08
madiv9820 <themathur901@gmail.com>
2025-08-08 02:03:00 +0530

    Adding route for ingesting data

--- ingestion_api/run.py
+++ ingestion_api/run.py
@@ -8,3 +8,14 @@ app.include_router(ingest_routes.router, prefix = '/ingest', tags = ['Ingestion'
 @app.get("/health")
 def health_check():
     return {"status": "Metropulse API is UP"}
+
+'''
+curl -X POST "http://localhost:8000/ingest/local/traffic" -H "Content-Type: application/json" -d '{"sensor_id":"T123","timestamp":"2025-08-08T10:30:00","vehicle_count":120,"avg_speed":45.5}'
+curl -X POST "http://localhost:8000/ingest/db/traffic" -H "Content-Type: application/json" -d '{"sensor_id":"T123","timestamp":"2025-08-08T10:30:00","vehicle_count":120,"avg_speed":45.5}'
+
+curl -X POST "http://localhost:8000/ingest/local/pollution" -H "Content-Type: application/json" -d '{"sensor_id":"P456","timestamp":"2025-08-08T12:00:00","pm25":35.2,"pm10":55.4,"no2":24.8}'
+curl -X POST "http://localhost:8000/ingest/db/pollution" -H "Content-Type: application/json" -d '{"sensor_id":"P456","timestamp":"2025-08-08T12:00:00","pm25":35.2,"pm10":55.4,"no2":24.8}'
+
+curl -X POST "http://localhost:8000/ingest/local/weather" -H "Content-Type: application/json" -d '{"sensor_id":"W789","timestamp":"2025-08-08T12:30:00","temperature":30.5,"humidity":60.2,"wind_speed":12.4}'
+curl -X POST "http://localhost:8000/ingest/db/weather" -H "Content-Type: application/json" -d '{"sensor_id":"W789","timestamp":"2025-08-08T12:30:00","temperature":30.5,"humidity":60.2,"wind_speed":12.4}'
+'''
