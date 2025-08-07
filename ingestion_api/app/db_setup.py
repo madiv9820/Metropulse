4d96b3679adc04751fb9f03902e5d3c9259062db
madiv9820 <themathur901@gmail.com>
2025-08-08 02:01:03 +0530

    Script to create DB and tables

---
+++ ingestion_api/app/db_setup.py
@@ -0,0 +1,47 @@
+import sqlite3
+
+def create_tables():
+    conn = sqlite3.connect("/home/madiv/Study/Projects/Metropulse/ingestion_api/data/db/metropulse.db")
+    cur = conn.cursor()
+
+    # Traffic sensor table
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS traffic (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            sensor_id TEXT NOT NULL,
+            timestamp TEXT NOT NULL,
+            vehicle_count INTEGER,
+            avg_speed REAL
+        );
+    """)
+
+    # Pollution sensor table
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS pollution (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            sensor_id TEXT NOT NULL,
+            timestamp TEXT NOT NULL,
+            pm25 REAL,
+            pm10 REAL,
+            no2 REAL
+        );
+    """)
+
+    # Weather sensor table
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS weather (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            sensor_id TEXT NOT NULL,
+            timestamp TEXT NOT NULL,
+            temperature REAL,
+            humidity REAL,
+            wind_speed REAL
+        );
+    """)
+
+    conn.commit()
+    conn.close()
+    print("✅ Tables created successfully.")
+
+if __name__ == "__main__":
+    create_tables()
