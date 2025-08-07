8312f4dfa0e981e389438b9ce441b61b75191815
madiv9820 <themathur901@gmail.com>
2025-08-08 00:57:06 +0530

    Adding new API post request to ingest raw data on local

--- ingestion_api/app/routes/ingest_routes.py
+++ ingestion_api/app/routes/ingest_routes.py
@@ -1,7 +1,28 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, Body
+from app.models import TrafficSensorData, PollutionSensorData, WeatherSensorData
+from app.utils.file_writer import save_to_raw_storage
 
 router = APIRouter()
 
 @router.get("/api/v1/hello")
 def hello():
     return {"message": "Welcome to Metropulse Ingestion API"}
+
+@router.post('/{sensor_type}')
+def ingest_data(sensor_type: str, payload: dict = Body(...)):
+    model_map = {
+        'traffic': TrafficSensorData,
+        'pollution': PollutionSensorData,
+        'weather': WeatherSensorData
+    }
+
+    if sensor_type not in model_map:
+        raise HTTPException(status_code = 400, detail = 'Invalid sensor_type')
+    
+    try:
+        validated_data = model_map[sensor_type](**payload)
+    except Exception as e:
+        raise HTTPException(status_code = 422, detail = f'Validation Error: {str(e)}')
+    
+    save_to_raw_storage(sensor_type, validated_data.dict())
+    return {'status': 'success', 'message': f'{sensor_type} data ingested'}
